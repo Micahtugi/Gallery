ddbc0815d6a594b123597c875f3b7205ad9b135f
Micah <micahkimathi@gmail.com>
2019-06-16 18:03:21 +0300

    Defined bootstrap3 in the settings.py file

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'photos.apps.PhotosConfig',
-
+    'photos',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
