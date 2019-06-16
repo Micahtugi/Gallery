d308fa38afd41af703b703517f552f73fa141ac8
Micah <micahkimathi@gmail.com>
2019-06-16 20:02:21 +0300

    Tested the save model

--- photos/models.py
+++ photos/models.py
@@ -5,6 +5,10 @@ class Editor(models.Model):
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
     
+    def __str__(self):
+        return self.first_name
+    def save_editor(self):
+        self.save()
 class tags(models.Model):
     name = models.CharField(max_length =30)
     
--- photos/tests.py
+++ photos/tests.py
@@ -10,4 +10,10 @@ class EditorTestClass(TestCase):
     # Testing instance
     def test_instance(self):
         self.assertTrue(isinstance(self.micah,Editor))
+        
+    # Testing Save Method
+    def test_save_method(self):
+        self.micah.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
 
