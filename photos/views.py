e731ae0a36281ff745015ffd6a56415d696343c4
Micah <micahkimathi@gmail.com>
2019-06-16 17:55:31 +0300

    Configured the welcome.html template in the views.py file

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def welcome(request):
-    return HttpResponse('Welcome to the Photo-Gallery')
+    return render(request, 'welcome.html')
 
