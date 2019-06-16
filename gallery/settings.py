b6428f989b77ce4780ea51f6da1bc5e5f0eab63a
Micah <micahkimathi@gmail.com>
2019-06-16 18:19:20 +0300

    Created the navbar.html file in the templates folder

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'photos.apps.PhotosConfig',
-    'photos',
+    # 'photos',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
