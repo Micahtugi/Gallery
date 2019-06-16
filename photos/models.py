3653f515fb7a39e986552d0452ea64c92fdbdd65
Micah <micahkimathi@gmail.com>
2019-06-16 19:40:22 +0300

    Defined a one to many relationship in the models.py file

--- photos/models.py
+++ photos/models.py
@@ -4,3 +4,14 @@ class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.name
+    
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
