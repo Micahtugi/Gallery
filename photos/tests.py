0a06522aa13b614d2ee5611ff9d6e53d460024e4
Micah <micahkimathi@gmail.com>
2019-06-16 19:56:15 +0300

    Run tests

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
 
-# Create your tests here.
+class EditorTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.micah= Editor(first_name = 'Micah', last_name ='Mutugi', email = 'micahkimathi@gmail.com')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.micah,Editor))
+
