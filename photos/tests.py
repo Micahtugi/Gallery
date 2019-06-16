a9722877beb51280cd2da2e9b3a7267bb412237a
Micah <micahkimathi@gmail.com>
2019-06-16 23:57:45 +0300

    Created a test class for the article model

--- photos/tests.py
+++ photos/tests.py
@@ -17,3 +17,21 @@ class EditorTestClass(TestCase):
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
 
+class ArticleTestClass(TestCase):
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.micah= Editor(first_name = 'Micah', last_name ='Mutugi',email ='micahkimathi@gmail.com')
+        self.micah.save_editor()
+        
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+        
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.micah)
+        self.new_article.save()
+        self.new_article.tags.add(self.new_tag)
+        
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime as dt
 
 def welcome(request):
     return render(request, 'welcome.html')
 
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+    
