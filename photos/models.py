223db3956441a75fc6d4d00a5ee5d8c51f00b988
Micah <micahkimathi@gmail.com>
2019-06-17 07:40:39 +0300

    Fixed a bug in rendering the template

--- photos/models.py
+++ photos/models.py
@@ -22,6 +22,7 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/')
     
     @classmethod
     def search_by_title(cls,search_term):
